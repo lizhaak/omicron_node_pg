da7aa448a4a20fec66e96475131ee8d5b75726de
Liz Haakenson <lhaakens3@gmail.com>
2016-08-17 15:29:54 -0500

    PUT and DELETE lecture

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 
   // add a book
   $('#book-submit').on('click', postBook);
+  $('#book-list').on('click', '.update', putBook);
+  $('#book-list').on('click', '.delete', deleteBook);
 });
 /**
  * Retrieve books from server and append to DOM
@@ -13,14 +15,43 @@ function getBooks() {
     url: '/books',
     success: function (books) {
       console.log('GET /books returns:', books);
+
       books.forEach(function (book) {
-        var $el = $('<li></li>');
-        $el.append('<strong>' + book.title + '</strong>');
-        $el.append(' <em>' + book.author + '</em>');
-        $el.append(' <time>' + book.published + '</time>');
-        $el.append(' <span>' + book.edition + '</span>');
-        $el.append(' <span>' + book.publisher + '</span>');
+        var $el = $('<div></div>');
+
+        var bookProperties = ['title', 'author', 'published', 'edition', 'publisher', 'genre'];
+
+        bookProperties.forEach(function (property) {
+          if (property == 'published') {
+                      // inputType = 'date';
+                      // book[property] = new Date(book[property]);
+                      book[property] = new Date(book[property]);
+
+                      //get strings for month/day/year
+                      var month = book[property].getUTCMonth(book[property]) + 1; //months from 1-12
+                      var day = book[property].getUTCDate(book[property]);
+                      var year = book[property].getUTCFullYear(book[property]);
+
+                      //catcatcanate into one string month/day/year and set to book.published as text
+                      book[property] = month + "/" + day + "/" + year;
+          }
+          var $input = $('<input type="text" id="' + property + '" name="' + property + '" />');
+          $input.val(book[property]);
+          $el.append($input);
+        });
+
+        // Do above instead of this below: bookProperties:
+        // $el.append('<strong>' + book.title + '</strong>');
+        // $el.append(' <em>' + book.author + '</em>');
+        // $el.append(' <time>' + book.published + '</time>');
+        // $el.append(' <span>' + book.edition + '</span>');
+        // $el.append(' <span>' + book.publisher + '</span>');
+        $el.data('bookId', book.id);
+        $el.append('<button class="update">Update</button>');
+        $el.append('<button class="delete">Delete</button>');
+
         $('#book-list').append($el);
+
       });
     },
 
@@ -56,4 +87,50 @@ function postBook() {
       console.log('POST /books does not work...');
     },
   });
+}
+
+function putBook() {
+  var book = {};
+  var inputs = $(this).parent().children().serializeArray();
+
+  $.each(inputs, function (i, field) {
+    book[field.name] = field.value;
+  });
+  console.log('book we are putting', book);
+
+  var bookId = $(this).parent().data('bookId');
+
+  $.ajax({
+    type: 'PUT',    //update a resource
+    url: '/books/' + bookId,
+    data: book,
+    success: function () {
+      $('#book-list').empty();
+      getBooks();
+    },
+
+    error: function () {
+      console.log('Error PUT /books/' + bookId);
+    },
+  });
+
+}
+
+function deleteBook() {
+  var bookId = $(this).parent().data('bookId');
+
+  $.ajax({
+    type: 'DELETE',
+    url: '/books/' + bookId,
+    success: function () {
+      console.log('DELETE success');
+      $('#book-list').empty();
+      getBooks();
+    },
+
+    error: function () {
+      console.log('DELETE failed');
+    }
+  })
+
 }
--- server/routes/books.js
+++ server/routes/books.js
@@ -34,9 +34,9 @@ router.post('/', function (req, res) {
       res.sendStatus(500);
     }
 
-    client.query('INSERT INTO books (author, title, published, edition, publisher)'
-                + 'VALUES ($1, $2, $3, $4, $5)',  //$1 will match book.author, etc.
-                [book.author, book.title, book.published, book.edition, book.publisher], //book object pushing values into an array
+    client.query('INSERT INTO books (author, title, published, edition, publisher, genre)'
+                + 'VALUES ($1, $2, $3, $4, $5, $6)',  //$1 will match book.author, etc.
+                [book.author, book.title, book.published, book.edition, book.publisher, book.genre], //book object pushing values into an array
                 function (err, result) {
                   done();
 
@@ -47,6 +47,62 @@ router.post('/', function (req, res) {
                   res.sendStatus(201);  //stands for Created something via our POST
                 });
   });
+});
+
+router.put('/:id', function (req, res) {
+  var id = req.params.id;  //id in orange needs to match :id in green, these can be foo if I wanted
+  var book = req.body;
+
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+
+    client.query('UPDATE books ' +
+                  'SET author = $1, ' +
+                  'title = $2, ' +
+                  'published = $3, ' +
+                  'edition = $4, ' +
+                  'publisher = $5, ' +
+                  'genre = $6 ' +
+                  'WHERE id = $7',
+                [book.author, book.title, book.published, book.edition, book.publisher, book.genre, id],
+              function (err, result) {
+                done();
+
+                if(err) {
+                  console.log('err', err);
+                  res.sendStatus(500);
+                } else {
+                  res.sendStatus(200);
+                }
+              });
+  });
+});
+
+router.delete('/:id', function (req, res) {
+  var id = req.params.id;
+
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+
+    client.query('DELETE FROM books ' +
+                  'WHERE id = $1',
+                  [id],
+                  function (err, result) {
+                    done();
+
+                    if (err) {
+                      res.sendStatus(500);
+                      return;
+                    }
+
+                    res.sendStatus(200);
+                  });
+  });
+
 });
 
 module.exports = router;
